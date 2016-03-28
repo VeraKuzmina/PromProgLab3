3f9968d778acfe78af1ab29185a1fe19d493edef
Vera Kuzmina <vera-flashka@mail.ru>
2016-03-28 21:15:24 +0300

    Отрывок из "Белой Гвардии" Булгакова

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
-/**
- * Created by Вера on 28.03.2016.
- */
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
-public class Main {
+/**
+ * Вариант No1, 7, 13, 19, 25
+ *В программу передается текст из файла. В этом тексте должно быть подсчитано количество
+ *прилагательных, наречий и глаголов.
+ */
+public class Main
+{
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        List<String> lines = null;
+        try {
+            lines = Files.readAllLines(Paths.get("test"), StandardCharsets.UTF_8);
+            } catch (IOException e) {
+            e.printStackTrace();
+            }
+        int pril, glag, nar;
+        for(String line: lines)
+        {
+        }
     }
 }
