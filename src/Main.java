64d6186ccbb541ce893bfe551e978165c00cec01
Vera Kuzmina <vera-flashka@mail.ru>
2016-03-28 21:32:47 +0300

    Регулярные выражения объявляются как константы

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,12 @@ import java.util.regex.Pattern;
  */
 public class Main
 {
+    private final static String PRIL_PAT = "[а-яА-Я]{2,}(ый|ий|ого|его|ому|ему|ым|им|ом|ем" +
+            "|ая|яя|ой|ей|ую|юю|оу|ее|ые|ых|ым|ие|ич|им|ыми|ими)[ .,?)!]";
+    private final static String GLAG_PAT = "[а-яА-Я]{2,}(ем|ешь|ете|ет|ут|ют|им|ишь|ите|ит|ат|ят"+
+        "|л|ла|ло|ли|ть)([ .,?)!]|(ся))";
+    private final static String NAR_PAT = "[а-яА-Я]{2,}(о|а)[ .,?)!]";
+
     public static void main(String[] args) {
         List<String> lines = null;
         try {
@@ -20,11 +26,11 @@ public class Main
             e.printStackTrace();
             }
         if (lines == null) System.exit(1);
-        Pattern ppril = Pattern.compile("[а-яА-я](ый|ий|ого|его|ому|ему|ым|им|ом|ем" +
-                "|ая|яя|ой|ей|ую|юю|оу|ее|ые|ых|ым|ие|ич|им|ыми|ими)[ .,?)!]");
-        Pattern pglag = Pattern.compile("[а-яА-я](ем|ешь|ете|ет|ут|ют|им|ишь|ите|ит|ат|ят"+
-                "|л|ла|ло|ли)[ .,?)!]");
-        Pattern pnar = Pattern.compile("[а-яА-я]{2,}(о|а)[ .,?)!]");
+
+        Pattern ppril = Pattern.compile(PRIL_PAT);
+        Pattern pglag = Pattern.compile(GLAG_PAT);
+        Pattern pnar = Pattern.compile(NAR_PAT);
+
         Matcher m;
         int pril=0, glag=0, nar=0;
         for(String line: lines){
